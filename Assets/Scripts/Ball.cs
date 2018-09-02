6c68bdb152bb479a3c0d786884538218d4568312
Arif Budiman <arif.budiman41@ui.ac.id>
2018-09-02 12:17:02 +0700

    add ball to schene

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private Vector2 topCorner, bottomCorner;
+
+	void Awake() {
+		bottomCorner = ScreenBoundaries.CalcBottomCorner(transform.position);
+		topCorner = ScreenBoundaries.CalcTopCorner(transform.position);
 	}
 }
