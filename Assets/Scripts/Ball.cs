70c9bd36bd89622d65c8f2dae943ef562a6d9e5a
Arif Budiman <arif.budiman41@ui.ac.id>
2018-09-02 14:42:18 +0700

    Add boundaries ball v1

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	public float speed;
 	private Vector2 topCorner, bottomCorner;
+	private Rigidbody2D rigid2D;
 
 	void Awake() {
-		bottomCorner = ScreenBoundaries.CalcBottomCorner(transform.position);
-		topCorner = ScreenBoundaries.CalcTopCorner(transform.position);
+		rigid2D = GetComponent<Rigidbody2D>();
 	}
+
+	void Start() {
+		rigid2D.AddForce(Vector2.up * speed);
+	}
+	
 }
---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour {
+	BoxCollider2D box2D;
+
+	void Awake()
+	{
+		transform.position = Vector3.zero;
+		// box2D = GetComponent<BoxCollider2D>();
+		// box2D.size = ScreenBoundaries.CalcTopCorner(transform.position) * 2;
+	}
+}
