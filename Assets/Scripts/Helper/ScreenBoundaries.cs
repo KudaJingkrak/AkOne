e7f4ab58741cf807512bcf3c4de220b25e4eba46
Arif Budiman <arif.budiman41@ui.ac.id>
2018-09-02 12:09:29 +0700

    'init'

---
+++ Assets/Scripts/Helper/ScreenBoundaries.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ScreenBoundaries {
+	public static Vector2 CalcTopCorner(Vector3 position) {
+		float camDistance = Vector3.Distance(position, Camera.main.transform.position);
+		return Camera.main.ViewportToWorldPoint(new Vector3(1,1, camDistance));
+	}
+
+	public static Vector2 CalcBottomCorner(Vector3 position) {
+		float camDistance = Vector3.Distance(position, Camera.main.transform.position);
+		return Camera.main.ViewportToWorldPoint(new Vector3(0,0, camDistance));
+	}
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	public float speed = 4.75f;
+
+	private float minX, maxX;
+	private BoxCollider2D box2D;
+	private Vector2 topCorner, bottomCorner;
+
+	public void ToLeft() {
+		if(minX < transform.position.x) {
+			Move(-1);
+		}
+	}
+
+	public void ToRight() {
+		if(transform.position.x < maxX) {
+			Move(1);
+		}
+	}
+
+	public void Move(float dir) {
+		transform.position = Vector3.Lerp(
+			transform.position, 
+			transform.position + (Vector3.right * dir), 
+			Time.deltaTime * speed
+		);
+	}
+
+	void Awake() {
+		box2D = GetComponent<BoxCollider2D>();
+		
+		bottomCorner = ScreenBoundaries.CalcBottomCorner(transform.position);
+		topCorner = ScreenBoundaries.CalcTopCorner(transform.position);
+		
+		float addition = box2D.size.x * transform.localScale.x / 2;
+		minX = bottomCorner.x + addition;
+		maxX = topCorner.x -  addition;
+	}
+
+	void Update() {
+
+		Vector3 pos = transform.position;
+
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			ToLeft();
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			ToRight();
+		}
+	}
+}
