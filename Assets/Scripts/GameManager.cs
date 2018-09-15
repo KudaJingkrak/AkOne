eee722a14e1e00598f6c2a8fbe48587c4f3f4f13
ArifBudiman19 <arif.budiman41@ui.ac.id>
2018-09-15 10:01:56 +0700

    add score and collision cound

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	private int _score = 0;
+	private float _collisionCount = 0f;
 	private List<Ball> _balls = new List<Ball>();
 	private Platform _platform = null;
 	private static GameManager _instance;
@@ -20,7 +22,13 @@ public class GameManager : MonoBehaviour {
 		return _instance._balls;
 	}
 
-	public Ball GetBall(int index) {
+	public Ball GetBall(int index = -1) {
+		if (index == -1) {
+			if(_instance._balls.Count > 0) {
+				return _instance._balls[0];
+			}
+		}
+
 		if(index < 0 || index > _instance._balls.Count) {
 			return null;
 		}
@@ -48,5 +56,25 @@ public class GameManager : MonoBehaviour {
 		_instance._balls.Clear();
 	}
 
+	public int Score{
+		get {
+			return _instance._score;
+		}
 
+		set {
+			_instance._score = value;
+		}
+	}
+
+	public void AddScore(int number) {
+		_instance._score += number;
+	}
+
+	public void AddCollisionCount() {
+		_instance._collisionCount += 1.0f / _instance._balls.Count;
+	}
+
+	public void ClearCollisionCOunt() {
+		_instance._collisionCount = 0f;
+	}
 }
